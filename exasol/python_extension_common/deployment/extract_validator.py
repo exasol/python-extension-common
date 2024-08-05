4ef28b8e7bed8b4ae6b4fb8c08b98f7b053122ba
ckunki <christoph.kuhnke@exasol.com>
2024-08-05 17:29:51 +0200

    Refactored extractvalidator to split total timeout
    for both potentially time-consuming phases
    P1) create UDF
    P2) check for manifest on all cluster nodes

--- exasol/python_extension_common/deployment/extract_validator.py
+++ exasol/python_extension_common/deployment/extract_validator.py
@@ -2,9 +2,9 @@ import re
 import exasol.bucketfs as bfs   # type: ignore
 import pyexasol     # type: ignore
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 from typing import Callable, List
-from tenacity import retry
+from tenacity import Retrying
 from tenacity.wait import wait_fixed
 from tenacity.stop import stop_after_delay
 
@@ -53,34 +53,50 @@ class ExtractValidator:
         self._interval = interval
         self._callback = callback if callback else lambda x, y: None
 
-    def _delete_manifest_udf(self, language_alias: str, schema: str):
-        self._pyexasol_conn.execute(f"DROP SCRIPT IF EXISTS {_udf_name(schema)}")
+    def _create_manifest_udf(self, language_alias: str, udf_name: str):
+        """
+        The SQL statements "ALTER SESSION SET SCRIPT_LANGUAGES" and "ALTER
+        SYSTEM SET SCRIPT_LANGUAGES" doe not check whether the specified
+        BucketFS path exists and has permissions allowing it to be accessed by
+        UDFs.
 
-    # The SQL statements "ALTER SESSION SET SCRIPT_LANGUAGES" and "ALTER
-    # SYSTEM SET SCRIPT_LANGUAGES" doe not check whether the specified
-    # BucketFS path exists and has permissions allowing it to be accessed by
-    # UDFs.
-    #
-    # Much more a later statement "CREATE SCRIPT" will fail with an error
-    # message.
-    #
-    # Hence we need to use a retry here, as well.
-    #
-    # Additionally we want to enable the user to specify a TOTAL timeout
-    # covering both retry phases: P1) CREATE SCRIPT P2) Check if UDF in SLC
-    # can be executed and finds extracted MANIFEST_FILE on each node of the database cluster.
-    def _create_manifest_udf(self, language_alias: str, schema: str):
-        self._pyexasol_conn.execute(
+        Much more a later statement "CREATE SCRIPT" will fail with an error
+        message.
+
+        Hence we need to use a retry here, as well.
+
+        Additionally we want to enable the user to specify a TOTAL timeout
+        covering both retry phases: P1) CREATE SCRIPT P2) Check if UDF in SLC
+        can be executed and finds extracted MANIFEST_FILE on each node of the database cluster.
+        """
+        try:
+            self._pyexasol_conn.execute(
+                f"""
+                CREATE OR REPLACE {language_alias} SET SCRIPT
+                    {udf_name}(my_path VARCHAR(256))
+                    EMITS (node INTEGER, manifest BOOL) AS
+                import os
+                def run(ctx):
+                    ctx.emit(exa.meta.node_id, os.path.isfile(ctx.my_path))
+                /
+                """
+            )
+        finally:
+            self._pyexasol_conn.execute(f"DROP SCRIPT IF EXISTS {udf_name}")
+
+    def _check_all_nodes(self, udf_name: str, nproc: int, manifest: str):
+        result = self._pyexasol_conn.execute(
             f"""
-            CREATE OR REPLACE {language_alias} SET SCRIPT
-                {_udf_name(schema)}(my_path VARCHAR(256))
-                EMITS (node INTEGER, manifest BOOL) AS
-            import os
-            def run(ctx):
-                ctx.emit(exa.meta.node_id, os.path.isfile(ctx.my_path))
-            /
+            SELECT {udf_name}({manifest})
+            FROM VALUES BETWEEN 1 AND {nproc} t(i) GROUP BY i
             """
-        )
+        ).fetchall()
+        pending = list( x[0] for x in result if not x[1] )
+        self._callback(nproc, pending)
+        if len(pending) > 0:
+            raise ExtractException(
+                f"{len(pending)} of {nproc} nodes are still pending."
+                f" IDs: {pending}")
 
     def verify_all_nodes(self, schema: str, language_alias: str, bfs_archive_path: bfs.path.PathLike):
         """
@@ -91,29 +107,26 @@ class ExtractValidator:
         archive or if after the configured timeout there are still nodes
         pending, for which the extraction could not be verified, yet.
         """
-        @retry(wait=wait_fixed(self._interval), stop=stop_after_delay(self._timeout), reraise=True)
-        def check_all_nodes(nproc, manifest):
-            result = self._pyexasol_conn.execute(
-                f"""
-                SELECT {_udf_name(schema)}({manifest})
-                FROM VALUES BETWEEN 1 AND {nproc} t(i) GROUP BY i
-                """
-            ).fetchall()
-            pending = list( x[0] for x in result if not x[1] )
-            self._callback(nproc, pending)
-            if len(pending) > 0:
-                raise ExtractException(
-                    f"{len(pending)} of {nproc} nodes are still pending."
-                    f" IDs: {pending}")
-
         manifest = f"{bfs_archive_path.as_udf_path()}/{MANIFEST_FILE}"
         if manifest is None:
             raise ExtractException(
                 f"{bfs_archive_path} does not point to an archive"
                 f" which could contain a file {MANIFEST_FILE}")
         nproc = self._pyexasol_conn.execute("SELECT nproc()").fetchone()
-        try:
-            self._create_manifest_udf(language_alias, schema)
-            check_all_nodes(nproc, manifest)
-        finally:
-            self._delete_manifest_udf(language_alias, schema)
+        udf_name = _udf_name(schema)
+        start = datetime.now()
+        for attempt in Retrying(
+                wait=wait_fixed(self._interval),
+                stop=stop_after_delay(self._timeout),
+                reraise=True):
+            with attempt:
+                self._create_manifest_udf(language_alias, udf_name)
+        elapsed = datetime.now() - start
+        remaining = self._timeout - elapsed
+        for attempt in Retrying(
+                wait=wait_fixed(self._interval),
+                stop=stop_after_delay(remaining),
+                reraise=True):
+            with attempt:
+                self._check_all_nodes(udf_name, nproc, manifest)
+
