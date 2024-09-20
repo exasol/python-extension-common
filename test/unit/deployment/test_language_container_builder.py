5d043673b666c5aa94f4734fa08bbed0db054bb0
mibe <mikhail.beck@exasol.com>
2024-09-20 12:36:20 +0100

    #60 Fixed the slc builder unit tests

--- test/unit/deployment/test_language_container_builder.py
+++ test/unit/deployment/test_language_container_builder.py
@@ -31,4 +31,4 @@ def test_copy_slc_flavor(tmp_path):
     copy_slc_flavor(tmp_path)
     flavor_base = tmp_path / 'flavor_base'
     assert flavor_base.exists()
-    assert len(list(flavor_base.rglob('*'))) == 8
+    assert len(list(flavor_base.rglob('*'))) == 7
