3449a91a01049cf4d301854b07e15b7c3356df2e
Ariel Schulz <ariel.schulz@exasol.com>
2025-04-11 13:10:48 +0200

    Add .poetry to ignored files for format check

--- noxconfig.py
+++ noxconfig.py
@@ -10,7 +10,7 @@ class Config:
     root: Path = ROOT_DIR
     doc: Path = ROOT_DIR / "doc"
     version_file: Path = ROOT_DIR / "version.py"
-    path_filters: Iterable[str] = ("dist", ".eggs", "venv", "metrics-schema")
+    path_filters: Iterable[str] = ("dist", ".eggs", "venv", "metrics-schema", ".poetry")
 
     exasol_versions = ["7.1.9"]
     python_versions = ["3.10", "3.11"]
