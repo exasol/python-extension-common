6b091b2a300d804f3b0c97a73c3006820178a703
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-07-22 12:19:45 +0200

    Prepare release 0.11.0

---
+++ doc/changes/changes_0.11.0.md
@@ -0,0 +1,9 @@
+# 0.11.0 - 2025-07-22
+
+## Refactorings
+
+ - #106: Updated dependencies
+
+## Features
+
+ - #109: Added flag which allows to select compression strategy of SLC during export 
--- version.py
+++ version.py
@@ -8,7 +8,7 @@ If you need to change the version, do so in the pyproject.toml, e.g. by using
 """
 
 MAJOR = 0
-MINOR = 10
+MINOR = 11
 PATCH = 0
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 __version__ = VERSION
